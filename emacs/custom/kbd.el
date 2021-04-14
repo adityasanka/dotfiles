642c2baa20b8ad48ac99f086a04aa7fe5bf6098d
Aditya Sanka <aditya.sanka@outlook.com>
2021-04-14 18:22:57 +0400

    [Bug] Fix issues with loading sequence of hydra and general

--- emacs/custom/kbd.el
+++ emacs/custom/kbd.el
@@ -1,6 +1,19 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+;; Log commands in a buffer
+(use-package command-log-mode)
+
+;; Compose key sequences
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+;; Convenient key bindings 
 (use-package general
   :after (ivy counsel)
   :config  
@@ -10,21 +23,8 @@
 			 :prefix "C-SPC")
   (rune/leader-keys
    "t" '(:ignore t :which-key "toggles")
-   "tt" '(counsel-load-theme :which-key "choose theme")))
+   "tt" '(counsel-load-theme :which-key "choose theme")
+   "ts" '(hydra-text-scale/body :which-key "scale-text")))
 
 (general-define-key
  "C-M-j" 'counsel-switch-buffer)
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
-(rune/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale-text"))
-
-;; Log commands in a buffer
-(use-package command-log-mode)
