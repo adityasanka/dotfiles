47eef7737b5946902ad12530c026e787124a3599
Aditya Sanka <aditya.sanka@outlook.com>
2021-04-15 18:41:47 +0400

    [Emacs] Add support for editing markdown

--- emacs/init.el
+++ emacs/init.el
@@ -20,3 +20,17 @@
 (load-library "generic-completion") ;; enhanced completion with ivy and counsel
 (load-library "projects")           ;; project mgmt with projectile
 (load-library "org")                ;; org mode preferences
+(load-library "markdown")           ;; markdown support
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(markdown-mode which-key visual-fill-column use-package smooth-scrolling rainbow-delimiters org-bullets magit ivy-rich hydra helpful general exec-path-from-shell doom-themes doom-modeline counsel-projectile command-log-mode all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
