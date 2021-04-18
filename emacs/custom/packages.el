9535bac9b4dfbcbf0d02a1bf8aad5b55967c1c55
Aditya Sanka <adityasanka@cafu.com>
2021-04-18 18:32:54 +0530

    [Emacs] Remove `melpa-stable` mirror from package archives

--- emacs/custom/packages.el
+++ emacs/custom/packages.el
@@ -5,8 +5,7 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
 	("org" . "https://orgmode.org/elpa/")
-	("melpa" . "https://melpa.org/packages/")
-	("melpa-stable" . "https://stable.melpa.org/packages/")))
+	("melpa" . "https://melpa.org/packages/")))
 
 ;; Initialize package system
 (package-initialize)
