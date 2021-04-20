d3e05813babd750e5631d7eea8987f6b7df170dd
Aditya Sanka <adityasanka@cafu.com>
2021-04-21 01:30:19 +0530

    [Emacs] Initialize package sources and setup archives in `init.el`
    	- Refactor to use `use-package` instead of `load-library`

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,26 @@
+;; Initialize package sources
+(require 'package)
+
+;; Setup package archives
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("org" . "https://orgmode.org/elpa/")
+	("melpa" . "https://melpa.org/packages/")))
+
+;; Initialize package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install use-package if not already present
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Ensure that every package is installed
+(setq use-package-always-ensure t)
+
+;; Use separate file for storing customization info
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
@@ -11,16 +34,15 @@
 
 (add-to-list 'load-path (dot/custom-elisp-dir))
 
-(load-library "dot-packages")      ;; package management
-(load-library "dot-env")           ;; setup env variables
-(load-library "dot-startup")       ;; splash settings
-(load-library "dot-fs")            ;; file management
-(load-library "dot-visual")        ;; visual preferences
-(load-library "dot-fonts")         ;; font preferences
-(load-library "dot-kbd")           ;; keyboard preferences
-(load-library "dot-git")           ;; version control
-(load-library "dot-themes")        ;; doom modeline and themes
-(load-library "dot-ivy")           ;; enhanced completion with ivy and counsel
-(load-library "dot-projects")      ;; project mgmt with projectile
-(load-library "dot-org")           ;; org mode preferences
-(load-library "dot-md")            ;; markdown support
+(use-package "dot-env" :ensure nil)           ;; setup env variables
+(use-package "dot-startup" :ensure nil)       ;; splash settings
+(use-package "dot-fs" :ensure nil)            ;; file management
+(use-package "dot-visual" :ensure nil)        ;; visual preferences
+(use-package "dot-fonts" :ensure nil)         ;; font preferences
+(use-package "dot-kbd" :ensure nil)           ;; keyboard preferences
+(use-package "dot-git" :ensure nil)           ;; version control
+(use-package "dot-themes" :ensure nil)        ;; doom modeline and themes
+(use-package "dot-ivy" :ensure nil)           ;; enhanced completion with ivy and counsel
+(use-package "dot-projects" :ensure nil)      ;; project mgmt with projectile
+(use-package "dot-org" :ensure nil)           ;; org mode preferences
+(use-package "dot-md" :ensure nil)            ;; markdown support
