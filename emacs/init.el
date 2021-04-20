a80176bd54a4f1d15b16636c96f54d4727e8017d
Aditya Sanka <adityasanka@cafu.com>
2021-04-20 17:58:57 +0530

    [Emacs] Use separate file for storing customization info
    	- Keeps the initialization clean and friendly for git
    	- TODO: Create custom file if not available on init

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,6 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 (defun dot/dotfiles-dir ()
   "Return the absolute path of dotfiles dir."
   (file-name-directory (file-truename user-init-file)))
@@ -21,17 +24,3 @@
 (load-library "dot-projects")      ;; project mgmt with projectile
 (load-library "dot-org")           ;; org mode preferences
 (load-library "dot-md")            ;; markdown support
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(markdown-mode which-key visual-fill-column use-package smooth-scrolling rainbow-delimiters org-bullets magit ivy-rich hydra helpful general exec-path-from-shell doom-themes doom-modeline counsel-projectile command-log-mode all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
