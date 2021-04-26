76bfd14803faa246d7e99bf71bc34cb342eb1d6f
Aditya Sanka <adityasanka@cafu.com>
2021-04-26 18:30:06 +0530

    [Emacs] Setup vterm to use terminal from within Emacs

--- emacs/init.el
+++ emacs/init.el
@@ -47,6 +47,7 @@
 ;; Disable line numbers for some modes
 (dolist (mode `(org-mode-hook
 		term-mode-hook
+		vterm-mode-hook
 		shell-mode-hook
 		treemacs-mode-hook
 		eshell-mode-hook))
@@ -384,3 +385,21 @@
 (lsp-register-custom-settings
  '(("gopls.completeUnimported" t t)
    ("gopls.staticcheck" t t)))
+
+(use-package term
+  :config
+  (setq explicit-shell-file-name "zsh") ;; Change this to bash, etc
+  ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
+
+  ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
+
+(use-package eterm-256color
+  :hook (term-mode . eterm-256color-mode))
+
+(use-package vterm
+  :commands vterm
+  :config
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")  ;; Set this to match your custom shell prompt
+  (setq vterm-shell "zsh")                         ;; Set this to customize the shell to launch
+  (setq vterm-max-scrollback 10000))
