8e8ebade8cfee47e3751a4b7b4d5160ed8296e27
Aditya Sanka <adityasanka@cafu.com>
2021-04-28 17:42:45 +0530

    [Emacs] Add conf to auto-update packages

--- emacs/init.el
+++ emacs/init.el
@@ -20,6 +20,15 @@
 ;; Ensure that every package is installed
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "21:00"))
+
 ;; Use separate file for storing customization info
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
