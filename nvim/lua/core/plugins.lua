58ff0fc3e10a1c00af677a00971bec9938d41810
Aditya Sanka <adityasanka93@gmail.com>
2024-01-17 13:48:30 +0400

    [Vim] Getting started with LSP integration

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -21,7 +21,13 @@ local plugins = {
     'nvim-telescope/telescope.nvim',
      tag = '0.1.5',
      dependencies = {'nvim-lua/plenary.nvim'}
+  },
+  -- LSP
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
   }
 }
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins)
