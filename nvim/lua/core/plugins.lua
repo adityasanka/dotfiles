9ea89fa1012fe9dbe18de24ad5a91e355f42c203
Aditya Sanka <adityasanka93@gmail.com>
2024-01-17 15:49:14 +0400

    [Vim] [Tmux] Seamless navigation between vim and tmux

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -28,7 +28,24 @@ local plugins = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   },
-  'lewis6991/gitsigns.nvim'
+  'lewis6991/gitsigns.nvim',
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 }
 
 require("lazy").setup(plugins)
