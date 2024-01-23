4839889a26b575035aca9c377f466d0bebb26efd
Aditya Sanka <adityasanka93@gmail.com>
2024-01-23 22:06:04 +0400

    [Vim] Refactor IDE settings
    * Improve code organization
    * Update LSP keybindings
    * Remove unused configuration for go lsp

--- nvim/lua/core/ide.lua
+++ nvim/lua/core/ide.lua
@@ -1,5 +1,15 @@
 ----------------------------------------------
--- improved highlighting
+-- git integration for buffers
+----------------------------------------------
+require('gitsigns').setup()
+
+----------------------------------------------
+-- code comments
+----------------------------------------------
+require('Comment').setup()
+
+----------------------------------------------
+-- improve code highlighting
 ----------------------------------------------
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
@@ -17,25 +27,17 @@ require 'nvim-treesitter.configs'.setup {
 }
 
 ----------------------------------------------
--- git integration for buffers
-----------------------------------------------
-require('gitsigns').setup()
-
-----------------------------------------------
--- code comments
-----------------------------------------------
-require('Comment').setup()
-
-----------------------------------------------
 -- lsp integration
 ----------------------------------------------
 
 -- easily install and manage lsp servers
 require('mason').setup()
+
 -- closes gap between mason and lspconfig
 require('mason-lspconfig').setup({
     ensure_installed = { 'lua_ls', 'gopls' },
 })
+
 -- quickstart configs for neovim lsp
 local lspconfig = require('lspconfig')
 
@@ -46,6 +48,56 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
     lsp_defaults.capabilities,
     require('cmp_nvim_lsp').default_capabilities()
 )
+
+-- attach keybindings
+vim.api.nvim_create_augroup("LspSettings", {})
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = 'LspSettings',
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf, noremap = true, silent = true }
+
+        -- apply code action if there is just one action
+        vim.keymap.set('n', '<space>ca', function()
+            vim.lsp.buf.code_action({ apply = true })
+        end, opts)
+
+        -- format buffer
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+
+        -- jump to definition
+        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', '<space>.', vim.lsp.buf.definition, opts)
+        -- jump to definition of the type of symbol under cursor
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        -- jump back
+        vim.keymap.set('n', '<space>,', '<C-O>', opts)
+
+        -- show information about the symbol under cursor
+        vim.keymap.set('n', '<space>h', vim.lsp.buf.hover, opts)
+
+        -- show implementations for the symbol under cursor
+        vim.keymap.set('n', '<space>i', vim.lsp.buf.implementation, opts)
+
+        -- renames all references to the symbol under the cursor.
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+
+        -- lists all the references to the symbol under the cursor
+        vim.keymap.set('n', '<space>rf', vim.lsp.buf.references, opts)
+
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+    end,
+})
 
 ----------------------------------------------
 -- lua ide
@@ -61,41 +113,13 @@ require("lspconfig").lua_ls.setup {
     }
 }
 
--- autoformat on save
+vim.api.nvim_create_augroup("LuaSettings", {})
 vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.lua',
+    group = 'LuaSettings',
     callback = function()
-        vim.lsp.buf.format {
-            async = false,
-        }
-    end,
-})
-
--- attach keybindings
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        -- autoformat on save
+        vim.lsp.buf.format({ async = false })
     end,
 })
 
@@ -106,11 +130,6 @@ local gopls_exec = 'gopls'
 if vim.fn.executable(gopls_exec) == 0 then
     vim.api.nvim_err_writeln(string.format('lsp: %q is not installed', gopls_exec))
     return
-end
-
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
 require("lspconfig").gopls.setup {
@@ -124,32 +143,17 @@ require("lspconfig").gopls.setup {
             gofumpt = true,
         },
     },
-    on_attach = on_attach
 }
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
+vim.api.nvim_create_augroup("GoSettings", {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    group = 'GoSettings',
     callback = function()
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { "source.organizeImports" } }
-        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-        -- machine and codebase, you may want longer. Add an additional
-        -- argument after params if you find that you have to write the file
-        -- twice for changes to be saved.
-        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-        for cid, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-                if r.edit then
-                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
-                end
-            end
-        end
+        -- autoformat on save
         vim.lsp.buf.format({ async = false })
-    end
+    end,
 })
-
 
 ----------------------------------------------
 -- autocomplete
