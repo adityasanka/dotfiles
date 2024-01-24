78a61621be821f1fbd9544835b5169d8b058ca58
Aditya Sanka <adityasanka93@gmail.com>
2024-01-24 14:20:31 +0400

    [Vim] Add keybinding to search lsp symbols

--- nvim/lua/core/navigation.lua
+++ nvim/lua/core/navigation.lua
@@ -58,5 +58,6 @@ end
 vim.keymap.set('n', '<leader>ff', find_in_project, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fc', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
