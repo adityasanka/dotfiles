b500997aec88922e0b951ba83f6ecae0c79ff94e
Aditya Sanka <adityasanka93@gmail.com>
2024-01-25 21:44:40 +0400

    [Vim] Update diagnostics signs

--- nvim/lua/core/ide.lua
+++ nvim/lua/core/ide.lua
@@ -228,3 +228,20 @@ cmp.setup({
         { name = 'buffer' },
     }),
 })
+
+----------------------------------------------
+-- diagnostics signs
+----------------------------------------------
+local signs = {
+    Error = '',
+    Warn = '',
+    Warning = '',
+    Hint = '',
+    Info = '',
+    Information = '',
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
