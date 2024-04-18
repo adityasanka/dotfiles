a83b594124f2526ee85eb5df929d798033bdd5c5
Aditya Sanka <adityasanka93@gmail.com>
2024-04-18 14:02:58 +0400

    [Vim] Special settings for go lsp

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -131,6 +131,94 @@ return {
 					},
 				})
 			end,
+			-- configure go server (with special settings)
+			["gopls"] = function()
+				local gopls_exec = "gopls"
+				if vim.fn.executable(gopls_exec) == 0 then
+					vim.api.nvim_err_writeln(string.format("lsp: %q is not installed", gopls_exec))
+					return
+				end
+
+				-- extend auto-complete candidates
+				local lsp_defaults = lspconfig.util.default_config
+				lsp_defaults.capabilities = vim.tbl_deep_extend(
+					"force",
+					lsp_defaults.capabilities,
+					require("cmp_nvim_lsp").default_capabilities()
+				)
+
+				lspconfig.gopls.setup({
+					cmd = { gopls_exec, "serve" },
+					capabilities = lsp_defaults.capabilities,
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+							},
+							staticcheck = true,
+							gofumpt = true,
+						},
+					},
+				})
+
+				vim.api.nvim_create_augroup("GoSettings", {})
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					pattern = "*.go",
+					group = "GoSettings",
+					callback = function()
+						-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+						local params = vim.lsp.util.make_range_params()
+						params.context = { only = { "source.organizeImports" } }
+						-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+						-- machine and codebase, you may want longer. Add an additional
+						-- argument after params if you find that you have to write the file
+						-- twice for changes to be saved.
+						-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+						local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+						for cid, res in pairs(result or {}) do
+							for _, r in pairs(res.result or {}) do
+								if r.edit then
+									local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+									vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								end
+							end
+						end
+
+						-- autoformat on save
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+
+				-- install golang ci lsp server
+				-- go install github.com/nametake/golangci-lint-langserver@latest
+				local configs = require("lspconfig/configs")
+
+				if not configs.golangcilsp then
+					configs.golangcilsp = {
+						default_config = {
+							cmd = { "golangci-lint-langserver" },
+							root_dir = require("lspconfig").util.root_pattern(".git", "go.mod"),
+							init_options = {
+								command = {
+									"golangci-lint",
+									"run",
+									"--enable-all",
+									"--disable",
+									"lll",
+									"--out-format",
+									"json",
+									"--issues-exit-code=1",
+								},
+							},
+						},
+					}
+				end
+
+				-- golangci-lint language server
+				lspconfig.golangci_lint_ls.setup({
+					filetypes = { "go", "gomod" },
+				})
+			end,
 		})
 	end,
 }
