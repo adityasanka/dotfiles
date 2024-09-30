6d6ced794197e3bfec677d06395fc5740dd899ca
Aditya Sanka <adityasanka93@gmail.com>
2024-09-30 22:50:12 +0400

    [Vim] Add "fancy" components to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,33 +1,47 @@
 return {
-    -- status bar
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local lazy_status = require("lazy.status")
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"meuter/lualine-so-fancy.nvim",
+	},
+	config = function()
+		local lazy_status = require("lazy.status")
 
-        require('lualine').setup({
-            options = {
-                icons_enabled = true,
-                theme = 'github_dark_default',
-            },
-            sections = {
-                lualine_a = {
-                    {
-                        'filename',
-                        path = 1,
-                    }
-                },
-                lualine_x = {
-                    {
-                        lazy_status.updates,
-                        cond = lazy_status.has_updates,
-                        color = { fg = "#ff9e64" },
-                    },
-                    { "encoding" },
-                    { "fileformat" },
-                    { "filetype" },
-                },
-            }
-        })
-    end
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+			},
+			sections = {
+				lualine_a = {
+					{ "fancy_mode", width = 3 },
+				},
+				lualine_b = {
+					{ "fancy_branch" },
+					{ "fancy_diff" },
+				},
+				lualine_c = {
+					{ "filename", path = 1 },
+					-- { "fancy_cwd", substitute_home = true },
+				},
+				lualine_x = {
+					{ "fancy_macro" },
+					{ "fancy_diagnostics" },
+					{ "fancy_searchcount" },
+					{ "fancy_location" },
+				},
+				lualine_y = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						-- color = { fg = "#ff9e64" },
+					},
+					{ "fancy_filetype", ts_icon = "" },
+				},
+				lualine_z = {
+					{ "fancy_lsp_servers" },
+				},
+			},
+		})
+	end,
 }
