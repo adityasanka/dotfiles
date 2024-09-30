be7026a70dfd8e94713b849e3a2a652a6aa35859
Aditya Sanka <adityasanka93@gmail.com>
2024-09-30 22:59:59 +0400

    [Vim] Remove circular icons from nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,46 +1,38 @@
 return {
-    -- file tree explorer
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 
-        "nvim-tree/nvim-web-devicons",
-        "projekt0n/circles.nvim",
-    },
-    config = function()
-        -- recommended settings from nvim-tree documentation
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+	-- file tree explorer
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        local circles = require('circles')
-        circles.setup({
-            icons = {
-                empty = '',
-                filled = '',
-                lsp_prefix = '' 
-            } 
-        })
+		require("nvim-tree").setup({
+			sort_by = "case_sensitive",
+			view = {
+				width = 40,
+				relativenumber = true,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		})
 
-        require("nvim-tree").setup({
-            sort_by = "case_sensitive",
-            view = {
-                width = 40,
-                relativenumber = true,
-            },
-            renderer = {
-                group_empty = true,
-                icons = {
-                    glyphs = circles.get_nvimtree_glyphs(),
-            },
-            },
-            filters = {
-                dotfiles = true,
-            },
-        })
-
-        -- set keymaps
-        local keymap = vim.keymap
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) 
-  end
+		-- set keymaps
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current file" }
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
 }
