335f7d606b3b639064bf9f6e32a51743b790ed08
Aditya Sanka <adityasanka93@gmail.com>
2024-10-15 23:43:31 +0400

    [Vim] Use catppuccin colorscheme
    Catppuccin uses italics for few highlight groups by default
    
    Configure conditionals and "@module" highlight group to use
    regular font so that italics are only used for code comments

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,24 @@
---[[ return {
-     -- github color scheme
-    'projekt0n/github-nvim-theme',
-    priority = 1000,
-    config = function()
-        vim.cmd('colorscheme github_dark_high_contrast')
-    end
-} ]]
-
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme catppuccin-mocha")
-
 		require("catppuccin").setup({
 			styles = {
 				comments = { "italic" },
+				-- conditionals defaults to italics
+				conditionals = {},
+			},
+			highlight_overrides = {
+				all = function()
+					return {
+						-- module-related keywords
+						["@module"] = { style = {} },
+					}
+				end,
 			},
 		})
+
+		vim.cmd("colorscheme catppuccin-mocha")
 	end,
 }
