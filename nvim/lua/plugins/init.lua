b4d19f898a959f87ae300c1abcee4e90168023b0
Aditya Sanka <adityasanka93@gmail.com>
2025-04-08 21:30:48 +0400

    [Vim] Organise navigation plugins into a separate directory
    * Update file names to describe functionality instead of the
    underlying plugins
    * Remove the `dressing` plugin. Contrary, to earlier assumption
    dressing plugin is not required for Telescope

--- nvim/lua/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-    -- lua functions that many plugins use
-    "nvim-lua/plenary.nvim",
-    -- seamless navigation between vim and tmux
-    "christoomey/vim-tmux-navigator"
-}
---
+++ nvim/lua/plugins/navigation/vim-tmux-navigation.lua
@@ -0,0 +1,4 @@
+return {
+	-- seamless navigation between vim and tmux
+	"christoomey/vim-tmux-navigator",
+}
