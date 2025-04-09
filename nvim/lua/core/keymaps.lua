b901387be410154e106d6190e430844ba9efd004
Aditya Sanka <adityasanka93@gmail.com>
2025-04-09 13:12:41 +0400

    [Vim] Set keymaps to switch between buffers
    * Remove un-used keymaps for tab management

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -20,12 +20,9 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
--- tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to prev tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+-- switch between windows
+keymap.set("n", "<Tab>", ":bnext<CR>")
+keymap.set("n", "<S-Tab>", ":bprevious<CR>")
 
 --  jump back to the previous location
 keymap.set("n", "<Leader>,", "<C-o>", { noremap = true, silent = true })
