02c6c85fe8221af391b2c3b795d98facbdddc2f3
Aditya Sanka <adityasanka93@gmail.com>
2025-04-10 16:41:34 +0400

    [Vim] Enable Lua module loader for faster startup time

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
+-- Put this at the *very* top of your init.lua
+-- Lua module loader only available in NeoVim 0.9.0+
+if vim.loader then
+	-- enable the Lua module loader for faster startup time
+	-- scans .lua files and caches the results in a smarter way.
+	vim.loader.enable()
+end
+
 require("core")
 require("plugin-manager")
