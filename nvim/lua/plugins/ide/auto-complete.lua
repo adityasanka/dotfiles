99d1bd31f89f82667ba2f6a1f0f8080f8723873e
Aditya Sanka <adityasanka93@gmail.com>
2025-04-16 19:10:05 +0400

    [Vim] Use blink.cmp instead nvim-cmp
    * Avoids the complexity of nvim-cmp's configuration by providing
    sensible defaults
    * Core sources (buffer, snippets, path, lsp) are built-in versus
    nvim-cmp's exclusively external sources
    * Presumably, better performance compared to nvim-cmp
    * Use default configuration from documentation
    https://cmp.saghen.dev/installation.html#lazy-nvim

--- nvim/lua/plugins/ide/auto-complete.lua
+++ nvim/lua/plugins/ide/auto-complete.lua
@@ -1,68 +1,49 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- source for text in buffer
-		"hrsh7th/cmp-buffer",
-		-- source for file system paths
-		"hrsh7th/cmp-path",
-		-- vs-code like pictograms
-		"onsails/lspkind.nvim",
+	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
+	dependencies = { "rafamadriz/friendly-snippets" },
+
+	-- use a release tag to download pre-built binaries
+	version = "1.*",
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = "default" },
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { documentation = { auto_show = false } },
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	config = function()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-
-		local is_cmp_enabled = true
-		vim.keymap.set("n", "<leader>cc", function()
-			is_cmp_enabled = not is_cmp_enabled
-			if is_cmp_enabled then
-				print("Autocomplete: ON")
-			else
-				print("Autocomplete: OFF")
-			end
-		end, { desc = "Toggle Autocomplete" })
-
-		cmp.setup({
-			enabled = function()
-				return is_cmp_enabled
-			end,
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			-- sources for autocompletion
-			sources = cmp.config.sources(
-				-- Primary sources
-				{
-					{ name = "copilot", max_item_count = 5 },
-					{ name = "nvim_lsp", max_item_count = 5 },
-				},
-				-- Fallback sources
-				{
-					{ name = "buffer", max_item_count = 3 },
-					{ name = "path", max_item_count = 3 },
-				}
-			),
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					maxwidth = 50,
-					ellipsis_char = "...",
-					symbol_map = { Copilot = "" },
-				}),
-				expandable_indicator = true,
-			},
-		})
-	end,
+	opts_extend = { "sources.default" },
 }
