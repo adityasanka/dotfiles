480fde8d5076ceac56521ce806d5fff882439735
Aditya Sanka <adityasanka93@gmail.com>
2025-09-26 18:33:05 +0530

    [Vim] Setup plugins to improve Markdown support

---
+++ nvim/.config/nvim/lua/plugins/ide/markdown.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+}
