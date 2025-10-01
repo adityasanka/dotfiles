978104154e44b258e2e81b51c97032fb00b21cff
Aditya Sanka <adityasanka93@gmail.com>
2025-10-01 19:07:28 +0530

    [Vim] Show hidden files by default in oil.nvim

--- nvim/.config/nvim/lua/plugins/navigation/file-explorer.lua
+++ nvim/.config/nvim/lua/plugins/navigation/file-explorer.lua
@@ -7,6 +7,9 @@ return {
 			-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
 			-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
 			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+			},
 		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
