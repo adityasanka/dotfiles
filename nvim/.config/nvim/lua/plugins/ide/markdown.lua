a040f4bbdc5f3ce37521582484abe56159d0316d
Aditya Sanka <adityasanka93@gmail.com>
2025-12-28 22:12:07 +0400

    [Vim] Remove render-markdown.nvim plugin
    The plugin is no longer needed for the markdown workflow.
    markdown-preview.nvim remains for browser-based preview.

--- nvim/.config/nvim/lua/plugins/ide/markdown.lua
+++ nvim/.config/nvim/lua/plugins/ide/markdown.lua
@@ -1,12 +1,5 @@
 return {
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	},
-	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		build = "cd app && yarn install",
