30bb5e84dbd05c3dba6294c005d5932c92279f11
Aditya Sanka <adityasanka93@gmail.com>
2026-01-27 17:24:02 +0400

    [Vim] Lazy-load zen-mode on keypress
    Defers zen-mode.nvim loading until <leader>z is pressed, reducing
    startup time. Converts config function to opts table.

--- nvim/.config/nvim/lua/plugins/ui/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/ui/zen-mode.lua
@@ -1,26 +1,21 @@
 return {
 	"folke/zen-mode.nvim",
-	config = function()
-		local zen = require("zen-mode")
-
-		zen.setup({
-			window = {
-				-- 80% editor width
-				width = 0.80,
-			},
-			on_open = function()
-				if vim.env.TMUX then
-					vim.fn.system("tmux set status off")
-				end
-			end,
-			on_close = function()
-				if vim.env.TMUX then
-					vim.fn.system("tmux set status on")
-				end
-			end,
-		})
-
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Toggle Zen Mode" })
-	end,
+	keys = {
+		{ "<leader>z", "<cmd>ZenMode<CR>", desc = "Toggle Zen Mode" },
+	},
+	opts = {
+		window = {
+			width = 0.80,
+		},
+		on_open = function()
+			if vim.env.TMUX then
+				vim.fn.system("tmux set status off")
+			end
+		end,
+		on_close = function()
+			if vim.env.TMUX then
+				vim.fn.system("tmux set status on")
+			end
+		end,
+	},
 }
