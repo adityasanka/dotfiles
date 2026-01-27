4b51e6442083274feb5f7f94749c6fa7b5cf7a7b
Aditya Sanka <adityasanka93@gmail.com>
2026-01-27 18:51:57 +0400

    [Vim] Hide utility keybindings from which-key menu
    Hide increment/decrement, go-to-definition, and jump-back keys
    from the which-key popup to reduce clutter while keeping them
    functional.

--- nvim/.config/nvim/lua/plugins/ui/which-key.lua
+++ nvim/.config/nvim/lua/plugins/ui/which-key.lua
@@ -11,6 +11,10 @@ return {
 			{ "<leader>t", group = "Toggle" },
 			{ "<leader>w", group = "Session" },
 			{ "<leader>x", group = "Diagnostics" },
+			{ "<leader>+", hidden = true, desc = "Increment number" },
+			{ "<leader>-", hidden = true, desc = "Decrement number" },
+			{ "<leader>.", hidden = true, desc = "Go to definition" },
+			{ "<leader>,", hidden = true, desc = "Jump back" },
 		},
 	},
 }
