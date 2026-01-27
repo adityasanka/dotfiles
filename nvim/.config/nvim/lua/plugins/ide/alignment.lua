e4f41ec3fb4e5500a4906c23332414d6c654a564
Aditya Sanka <adityasanka93@gmail.com>
2026-01-27 17:28:54 +0400

    [Vim] Lazy-load vim-easy-align on keypress
    Defers vim-easy-align loading until ga is pressed, reducing startup
    time. Simplifies config by using keys spec only.

--- nvim/.config/nvim/lua/plugins/ide/alignment.lua
+++ nvim/.config/nvim/lua/plugins/ide/alignment.lua
@@ -1,11 +1,7 @@
 return {
 	"junegunn/vim-easy-align",
-	event = "VeryLazy",
-	config = function()
-		-- Start interactive EasyAlign in visual mode (e.g. vipga)
-		vim.keymap.set("v", "ga", "<Plug>(EasyAlign)", { desc = "Easy Align" })
-
-		-- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-		vim.keymap.set("n", "ga", "<Plug>(EasyAlign)", { desc = "Easy Align" })
-	end,
+	keys = {
+		{ "ga", "<Plug>(EasyAlign)", mode = "v", desc = "Easy Align" },
+		{ "ga", "<Plug>(EasyAlign)", mode = "n", desc = "Easy Align" },
+	},
 }
