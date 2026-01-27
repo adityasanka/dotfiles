759b8e9f067e4e2dbe92fc4f68f895d01a79ab87
Aditya Sanka <adityasanka93@gmail.com>
2026-01-27 22:13:10 +0400

    [Vim] Move zen mode keybinding to <leader>tz
    Change zen mode toggle from <leader>z to <leader>tz for
    consistent grouping under the Toggle menu.

--- nvim/.config/nvim/lua/plugins/ui/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/ui/zen-mode.lua
@@ -1,7 +1,7 @@
 return {
 	"folke/zen-mode.nvim",
 	keys = {
-		{ "<leader>z", "<cmd>ZenMode<CR>", desc = "Toggle Zen Mode" },
+		{ "<leader>tz", "<cmd>ZenMode<CR>", desc = "Toggle Zen Mode" },
 	},
 	opts = {
 		window = {
