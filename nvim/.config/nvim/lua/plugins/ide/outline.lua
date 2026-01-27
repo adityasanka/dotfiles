8a6f81004523db835466dbbb50ca626e0d32a342
Aditya Sanka <adityasanka93@gmail.com>
2026-01-27 17:27:20 +0400

    [Vim] Lazy-load aerial on keypress
    Defers aerial.nvim loading until <leader>a is pressed. Removes
    open_automatic feature as it requires early loading.

--- nvim/.config/nvim/lua/plugins/ide/outline.lua
+++ nvim/.config/nvim/lua/plugins/ide/outline.lua
@@ -1,40 +1,21 @@
 return {
 	"stevearc/aerial.nvim",
-	event = "VeryLazy",
+	keys = {
+		{ "<leader>a", "<cmd>AerialToggle!<CR>", desc = "Toggle Aerial" },
+		{ "<leader>A", "<cmd>AerialNavToggle<CR>", desc = "Toggle Aerial Nav" },
+	},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
 	},
-	config = function()
-		require("aerial").setup({
-			-- Priority list of preferred backends
-			backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
-
-			layout = {
-				default_direction = "prefer_right",
-				min_width = 40,
-				max_width = { 80, 0.3 },
-				resize_to_content = true,
-			},
-
-			show_guides = true,
-
-			-- Auto-open for files > 100 lines, but only for specific filetypes
-			open_automatic = function(bufnr)
-				local line_count = vim.api.nvim_buf_line_count(bufnr)
-				local filetype = vim.bo[bufnr].filetype
-
-				local supported_filetypes = {
-					"go",
-					"lua",
-				}
-
-				return line_count > 100 and vim.tbl_contains(supported_filetypes, filetype)
-			end,
-		})
-
-		-- Keymaps
-		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial" })
-		vim.keymap.set("n", "<leader>A", "<cmd>AerialNavToggle<CR>", { desc = "Toggle Aerial Nav" })
-	end,
+	opts = {
+		backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+		layout = {
+			default_direction = "prefer_right",
+			min_width = 40,
+			max_width = { 80, 0.3 },
+			resize_to_content = true,
+		},
+		show_guides = true,
+	},
 }
